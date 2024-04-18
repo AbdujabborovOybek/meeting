c7877475d4f1172cf8cce511db6f1e43d2c45e0d
AbdujabborovOybek <abdujabborovoybek@gmail.com>
2024-04-18 23:30:14 +0500

    fix

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import cv2
+import socketio
+import base64
+import io
+
+# Socket.io ulanish
+sio = socketio.Client()
+
+# Serverga ulanish
+sio.connect('http://localhost:3000')
 
 # Veb kamerani tanlash
 cap = cv2.VideoCapture(0)
@@ -12,8 +21,14 @@ while True:
     # Kameradan rasm olish
     ret, frame = cap.read()
 
-    # Rasmni ko'rsatish
-    cv2.imshow('Web Kamera', frame)
+    # Tasvirni byte-objektga aylantirish
+    img_bytes = cv2.imencode('.jpg', frame)[1].tobytes()
+
+    # Tasvirlarni base64 formatga o'tkazish
+    img_base64 = base64.b64encode(img_bytes).decode('utf-8')
+
+    # Tasvirlarni yuborish
+    sio.emit('message', img_base64)
 
     # Chiqish uchun qo'shimcha tugma (q'oshimcha tugma bosilsa va "q" bosing)
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -24,3 +39,6 @@ cap.release()
 
 # Ekranlarni yopish
 cv2.destroyAllWindows()
+
+# Socket.io ulanishni yopish
+sio.disconnect()
